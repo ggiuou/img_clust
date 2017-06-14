596d70fcac8ad2fe7c31c2cf213b898fb7eb2952
ggiuou <nutchapol_s@hotmail.com>
2017-06-14 15:59:15 +0700

    rename

--- dlibtest/main.cpp
+++ dlibtest/main.cpp
@@ -1,30 +1,19 @@
-// The contents of this file are in the public domain. See LICENSE_FOR_EXAMPLE_PROGRAMS.txt
-/*
-This is an example illustrating the use of the deep learning tools from the dlib C++
-Library.  In it, we will show how to do face recognition.  This example uses the
-pretrained dlib_face_recognition_resnet_model_v1 model which is freely available from
-the dlib web site.  This model has a 99.38% accuracy on the standard LFW face
-recognition benchmark, which is comparable to other state-of-the-art methods for face
-recognition as of February 2017.
-
-In this example, we will use dlib to do face clustering.  Included in the examples
-folder is an image, bald_guys.jpg, which contains a bunch of photos of action movie
-stars Vin Diesel, The Rock, Jason Statham, and Bruce Willis.   We will use dlib to
-automatically find their faces in the image and then to automatically determine how
-many people there are (4 in this case) as well as which faces belong to each person.
-
-Finally, this example uses a network with the loss_metric loss.  Therefore, if you want
-to learn how to train your own models, or to get a general introduction to this loss
-layer, you should read the dnn_metric_learning_ex.cpp and
-dnn_metric_learning_on_images_ex.cpp examples.
-*/
-
 #include <dlib/gui_widgets.h>
 #include <dlib/clustering.h>
 #include <dlib/string.h>
 #include <dlib/dnn.h>
 #include <dlib/image_io.h>
 #include <dlib/image_processing/frontal_face_detector.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <direct.h>
+#include <iostream>
+#include <windows.h>
+#include <string>
+#include <vector>
+#include <stack>
 
 using namespace dlib;
 using namespace std;
@@ -76,52 +65,68 @@ std::vector<matrix<rgb_pixel>> jitter_image(
 	const matrix<rgb_pixel>& img
 );
 
-// ----------------------------------------------------------------------------------------
+bool ListFiles(wstring path, wstring mask, std::vector<wstring>& files);
 
+// ----------------------------------------------------------------------------------------
+struct info
+{
+	int count;
+	string name;
+};
+
+// ----------------------------------------------------------------------------------------
 int main(int argc, char** argv) try
 {
-	cout << argc << endl;
-	if (argc != 2)
-	{
-		cout << "Run this example by invoking it like this: " << endl;
-		cout << "   ./dnn_face_recognition_ex faces/bald_guys.jpg" << endl;
-		cout << endl;
-		cout << "You will also need to get the face landmarking model file as well as " << endl;
-		cout << "the face recognition model file.  Download and then decompress these files from: " << endl;
-		cout << "http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2" << endl;
-		cout << "http://dlib.net/files/dlib_face_recognition_resnet_model_v1.dat.bz2" << endl;
-		cout << endl;
-		return 1;
-	}
 
-	// The first thing we are going to do is load all our models.  First, since we need to
-	// find faces in the image we will need a face detector:
 	frontal_face_detector detector = get_frontal_face_detector();
-	// We will also use a face landmarking model to align faces to a standard pose:  (see face_landmark_detection_ex.cpp for an introduction)
+
 	shape_predictor sp;
-	deserialize("shape_predictor_68_face_landmarks.dat") >> sp;
-	// And finally we load the DNN responsible for face recognition.
+	wchar_t buffer[MAX_PATH];
+	GetModuleFileName(NULL, buffer, MAX_PATH);
+	wstring ws(buffer);
+	string path(ws.begin(), ws.end());
+	path = path.substr(0, path.find_last_of("\\/") + 1);
+	deserialize(path + "shape_predictor_68_face_landmarks.dat") >> sp;
+
 	anet_type net;
-	deserialize("dlib_face_recognition_resnet_model_v1.dat") >> net;
+	deserialize(path + "dlib_face_recognition_resnet_model_v1.dat") >> net;
 
-	matrix<rgb_pixel> img;
-	load_image(img, argv[1]);
-	// Display the raw image on the screen
-	image_window win(img);
+	std::vector<wstring> files;
+	const size_t size = strlen(argv[1]) + 1;
+	wchar_t* path_source = new wchar_t[size];
+	mbstowcs(path_source, argv[1], size);
 
-	// Run the face detector on the image of our action heroes, and for each face extract a
-	// copy that has been normalized to 150x150 pixels in size and appropriately rotated
-	// and centered.
 	std::vector<matrix<rgb_pixel>> faces;
-	for (auto face : detector(img))
+	std::vector<string> face_names;
+	//std::vector<image_window> win_detect(10);
+	int count = 0;
+	if (ListFiles(path_source, L"*", files))
 	{
-		auto shape = sp(img, face);
-		matrix<rgb_pixel> face_chip;
-		extract_image_chip(img, get_face_chip_details(shape, 150, 0.25), face_chip);
-		faces.push_back(move(face_chip));
-		// Also put some boxes on the faces so we can see that the detector is finding
-		// them.
-		win.add_overlay(face);
+		for (std::vector<wstring>::iterator it = files.begin();it != files.end(); ++it)
+		{
+			matrix<rgb_pixel> img;
+			string path_file_img(it->begin(), it->end());
+			load_image(img, path_file_img);
+
+			cout << "Load : " << path_file_img << endl;
+
+			string fName(path_file_img);
+			fName = fName.substr(0, fName.find_last_of("\\/"));
+			fName = fName.substr(fName.find_last_of("\\/"));
+			/*win_detect[count].set_title("face cluster " + cast_to_string(count));
+			win_detect[count].set_image(img);*/
+
+			for (auto face : detector(img))
+			{
+				auto shape = sp(img, face);
+				matrix<rgb_pixel> face_chip;
+				extract_image_chip(img, get_face_chip_details(shape, 150, 0.25), face_chip);
+				faces.push_back(move(face_chip));
+				face_names.push_back(fName);
+				//win_detect[count].add_overlay(face);
+			}
+			count++;
+		}
 	}
 
 	if (faces.size() == 0)
@@ -129,67 +134,51 @@ int main(int argc, char** argv) try
 		cout << "No faces found in image!" << endl;
 		return 1;
 	}
-
-	// This call asks the DNN to convert each face image in faces into a 128D vector.
-	// In this 128D vector space, images from the same person will be close to each other
-	// but vectors from different people will be far apart.  So we can use these vectors to
-	// identify if a pair of images are from the same person or from different people.  
 	std::vector<matrix<float, 0, 1>> face_descriptors = net(faces);
 
-
-	// In particular, one simple thing we can do is face clustering.  This next bit of code
-	// creates a graph of connected faces and then uses the Chinese whispers graph clustering
-	// algorithm to identify how many people there are and which faces belong to whom.
 	std::vector<sample_pair> edges;
 	for (size_t i = 0; i < face_descriptors.size(); ++i)
 	{
 		for (size_t j = i + 1; j < face_descriptors.size(); ++j)
 		{
-			// Faces are connected in the graph if they are close enough.  Here we check if
-			// the distance between two face descriptors is less than 0.6, which is the
-			// decision threshold the network was trained to use.  Although you can
-			// certainly use any other threshold you find useful.
-			if (length(face_descriptors[i] - face_descriptors[j]) < 0.6)
+			if (length(face_descriptors[i] - face_descriptors[j]) < (float)atof(argv[2]))
 				edges.push_back(sample_pair(i, j));
 		}
 	}
 	std::vector<unsigned long> labels;
 	const auto num_clusters = chinese_whispers(edges, labels);
-	// This will correctly indicate that there are 4 people in the image.
 	cout << "number of people found in the image: " << num_clusters << endl;
 
-
-	// Now let's display the face clustering results on the screen.  You will see that it
-	// correctly grouped all the faces. 
-	std::vector<image_window> win_clusters(num_clusters);
+	//std::vector<image_window> win_clusters(num_clusters);
+	array2d<rgb_pixel> img2d;
 	for (size_t cluster_id = 0; cluster_id < num_clusters; ++cluster_id)
 	{
 		std::vector<matrix<rgb_pixel>> temp;
+		string path_save(path + cast_to_string(cluster_id));
+		mkdir(path_save.c_str());
+
 		for (size_t j = 0; j < labels.size(); ++j)
 		{
 			if (cluster_id == labels[j])
+			{
 				temp.push_back(faces[j]);
+				assign_image(img2d, temp.back());
+				save_jpeg(img2d, path_save + "/" + face_names[j] + cast_to_string(j) + ".jpg");
+			}
+
 		}
-		win_clusters[cluster_id].set_title("face cluster " + cast_to_string(cluster_id));
-		win_clusters[cluster_id].set_image(tile_images(temp));
+		/*win_clusters[cluster_id].set_title("face cluster " + cast_to_string(cluster_id));
+		win_clusters[cluster_id].set_image(tile_images(temp));*/
 	}
 
+	cout << "success" << endl;
 
+	
 
+	/*cout << "face descriptor for one face: " << trans(face_descriptors[0]) << endl;
 
-	// Finally, let's print one of the face descriptors to the screen.  
-	cout << "face descriptor for one face: " << trans(face_descriptors[0]) << endl;
-
-	// It should also be noted that face recognition accuracy can be improved if jittering
-	// is used when creating face descriptors.  In particular, to get 99.38% on the LFW
-	// benchmark you need to use the jitter_image() routine to compute the descriptors,
-	// like so:
 	matrix<float, 0, 1> face_descriptor = mean(mat(net(jitter_image(faces[0]))));
-	cout << "jittered face descriptor for one face: " << trans(face_descriptor) << endl;
-	// If you use the model without jittering, as we did when clustering the bald guys, it
-	// gets an accuracy of 99.13% on the LFW benchmark.  So jittering makes the whole
-	// procedure a little more accurate but makes face descriptor calculation slower.
-
+	cout << "jittered face descriptor for one face: " << trans(face_descriptor) << endl;*/
 
 	cout << "hit enter to terminate" << endl;
 	cin.get();
@@ -205,8 +194,6 @@ std::vector<matrix<rgb_pixel>> jitter_image(
 	const matrix<rgb_pixel>& img
 )
 {
-	// All this function does is make 100 copies of img, all slightly jittered by being
-	// zoomed, rotated, and translated a little bit differently.
 	thread_local random_cropper cropper;
 	cropper.set_chip_dims(150, 150);
 	cropper.set_randomly_flip(true);
@@ -229,5 +216,45 @@ std::vector<matrix<rgb_pixel>> jitter_image(
 	return crops;
 }
 
-// ----------------------------------------------------------------------------------------
+bool ListFiles(wstring path, wstring mask, std::vector<wstring>& files) {
+	HANDLE hFind = INVALID_HANDLE_VALUE;
+	WIN32_FIND_DATA ffd;
+	wstring spec;
+	stack<wstring> directories;
 
+	directories.push(path);
+	files.clear();
+
+	while (!directories.empty()) {
+		path = directories.top();
+		spec = path + L"/" + mask;
+		directories.pop();
+
+		hFind = FindFirstFile(spec.c_str(), &ffd);
+		if (hFind == INVALID_HANDLE_VALUE) {
+			return false;
+		}
+
+		do {
+			if (wcscmp(ffd.cFileName, L".") != 0 &&
+				wcscmp(ffd.cFileName, L"..") != 0) {
+				if (ffd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
+					directories.push(path + L"/" + ffd.cFileName);
+				}
+				else {
+					files.push_back(path + L"/" + ffd.cFileName);
+				}
+			}
+		} while (FindNextFile(hFind, &ffd) != 0);
+
+		if (GetLastError() != ERROR_NO_MORE_FILES) {
+			FindClose(hFind);
+			return false;
+		}
+
+		FindClose(hFind);
+		hFind = INVALID_HANDLE_VALUE;
+	}
+
+	return true;
+}
